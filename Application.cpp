668fb1c36dd8612079edd9bd5d3fee0994349749
Yukijudaii <56759465+Jary-lrj@users.noreply.github.com>
2021-10-19 23:09:02 +0800

    主界面模板

---
+++ Application.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int login_status;//区分商家端还是客户端的选项代码，1代表用户，2代表商家。
+
+    //1 -> Users Mode, 2 -> Sellers Mode, 0 -> Exit, other code -> error message.
+    while (1) {
+
+        cout << "Welcome TMALL 11.11 Festival! You are? 1-User 2-Seller. Select your status to continue." << endl;
+
+        cin >> login_status;
+
+        if (login_status == 1) {
+            /*
+             * Client for users Code.
+             */
+        } else if (login_status == 2) {
+            /*
+             * Client for sellers Code.
+             */
+        } else if (login_status == 0) {
+            break;
+        } else {
+            cout << "We have no such login status. Please try again." << endl;
+        }
+    }
+    return 0;
+}
