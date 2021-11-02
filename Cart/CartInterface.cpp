cb05ba128e5c6d065b1a1a263ce07139ae8202d4
frederickmo <frederick_0428@tongji.edu.cn>
2021-11-02 16:41:30 +0800

    增加Cart子系统注释，修改文档中Cart子系统部分

--- Cart/CartInterface.cpp
+++ Cart/CartInterface.cpp
@@ -8,8 +8,11 @@ using namespace std;
 void CartInterface::MainInterface(shopInterface shopInterface, Customer *customer, OrderInterface &orderInterface) {
     int choice_status, ID;
     showUserCartCmds showUserCartCmds;
+    // 获取当前用户
     cart.setCustomer(customer);
+    // 获取商店信息
     cart.setShopInterface(shopInterface);
+    // 链接订单界面类
     cart.setOrderInterface(orderInterface);
     while (true) {
         showUserCartCmds.showUserCmds();
@@ -27,6 +30,7 @@ void CartInterface::MainInterface(shopInterface shopInterface, Customer *custome
                 cout << "请输入您要加入商品ID与数量: " << endl;
                 int addCommodityID, addCommodityNum;
                 cin >> addCommodityID >> addCommodityNum;
+                // 执行添加商品Command
                 AddCommodity addCommodity(addCommodityID, addCommodityNum);
                 addCommodity.setCart(cart);
                 addCommodity.operation();
@@ -37,6 +41,7 @@ void CartInterface::MainInterface(shopInterface shopInterface, Customer *custome
                 cout << "请输入您要删除商品的ID: " << endl;
                 int removeCommodityID;
                 cin >> removeCommodityID;
+                // 执行移除商品Command
                 RemoveCommodity removeCommodity(removeCommodityID);
                 removeCommodity.setCart(cart);
                 removeCommodity.operation();
@@ -47,6 +52,7 @@ void CartInterface::MainInterface(shopInterface shopInterface, Customer *custome
                 cout << "请输入您要删除商品的ID与要减少的数量: " << endl;
                 int removeCommodityID, removeCommodityNum;
                 cin >> removeCommodityID >> removeCommodityNum;
+                // 执行按一定数量移除商品Command
                 RemoveSomeCommodity removeSomeCommodity(removeCommodityID, removeCommodityNum);
                 removeSomeCommodity.setCart(cart);
                 removeSomeCommodity.operation();
@@ -59,6 +65,7 @@ void CartInterface::MainInterface(shopInterface shopInterface, Customer *custome
                 cout << "1-全部结算 2-结算指定ID全部商品 3-结算指定ID商品指定数量: " << endl;
                 cin >> choiceSub5;
                 if (choiceSub5 == 1) {
+                    // 执行全部结算Command
                     PayAll payAll;
                     payAll.setCart(cart);
                     payAll.operation();
@@ -67,6 +74,7 @@ void CartInterface::MainInterface(shopInterface shopInterface, Customer *custome
                 } else if (choiceSub5 == 2) {
                     cout << "请输入要结算商品的ID: " << endl;
                     cin >> payCommodityID;
+                    // 执行结算单件商品Command
                     PaySingleCommodity paySingleCommodity(payCommodityID);
                     paySingleCommodity.setCart(cart);
                     paySingleCommodity.operation();
@@ -76,6 +84,7 @@ void CartInterface::MainInterface(shopInterface shopInterface, Customer *custome
                     cout << "请输入要结算商品的ID与数量: " << endl;
                     cin >> payCommodityID;
                     cin >> payCommodityNum;
+                    // 执行按一定数量结算商品Command
                     PaySomeCommodity paySomeCommodity(payCommodityID, payCommodityNum);
                     paySomeCommodity.setCart(cart);
                     paySomeCommodity.operation();
